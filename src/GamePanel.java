da6db3f9da6a0ee058e08a59032063c60147032d
audreychan <league@WTS22.attlocal.net>
2019-05-05 11:29:04 -0700

    In Testing

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -13,22 +13,22 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
 
 	Font titleFont;
 	Font textFont;
-	
+
 	Timer timer;
-	
+
 	final int titleState = 0;
 	final int gameState = 1;
 	final int endState = 2;
-	final int icState = 3; //ic stands for instruction/credits
+	final int icState = 3; // ic stands for instruction/credits
 	int currentState = titleState;
-	
+
 	Paddle right = new Paddle(10, 350, 10, 100);
 	Paddle left = new Paddle(FourPong.length - 20, 350, 10, 100);
 	Paddle up = new Paddle(350, 10, 100, 10);
 	Paddle down = new Paddle(350, FourPong.length - 20, 100, 10);
 	Ball ball = new Ball(388, 388, 15, 15);
 	Manager manager = new Manager(right, left, up, down, ball, this);
-	
+
 	boolean isWPressed = false;
 	boolean isSPressed = false;
 	boolean isAPressed = false;
@@ -37,102 +37,140 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
 	boolean isDownPressed = false;
 	boolean isLeftPressed = false;
 	boolean isRightPressed = false;
-	
+
 	boolean isPaused = false;
 	double xPauseSpeed;
 	double yPauseSpeed;
-	
+
 	public GamePanel() {
 		titleFont = new Font("Arial", Font.PLAIN, 48);
 		textFont = new Font("Arial", Font.PLAIN, 24);
 		timer = new Timer(1000 / 60, this);
 		timer.start();
 	}
-	
+
 	void updateGameState() {
 		manager.update();
 		ball();
 		move();
 	}
-	
+
 	void drawTitleState(Graphics g) {
 		g.setColor(new Color(0, 10, 45));
 		g.fillRect(0, 0, FourPong.length, FourPong.length);
-		
+
 		g.setColor(Color.white);
 		g.setFont(textFont);
 		g.drawString("Welcome to", 325, 40);
 		g.drawString("Press SPACE to start.", 270, 270);
 		g.drawString("Press B to get instructions.", 250, 300);
-		
+		g.drawString("Warning: The keys may stop working.", 200, 370);
+
 		g.setFont(titleFont);
 		g.drawString("4 Way Pong", 250, 100);
 	}
-	
+
 	void drawGameState(Graphics g) {
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, FourPong.length, FourPong.length);
+
+		g.setColor(Color.white);
+		g.setFont(textFont);
+		g.drawString("Score: " + manager.score, 20, 40);
+
 		manager.draw(g);
 	}
-	
+
 	void drawEndState(Graphics g) {
 		g.setColor(new Color(33, 0, 0));
 		g.fillRect(0, 0, FourPong.length, FourPong.length);
+
+		g.setColor(Color.white);
+		g.setFont(titleFont);
+		g.drawString("Game Over!", 255, 100);
+
+		g.setFont(textFont);
+		g.drawString("Press SPACE to play again.", 245, 400);
+		g.drawString("Press B to get instructions.", 250, 430);
+
+		if (manager.score == 0) {
+			g.drawString("You got no points.", 290, 270);
+		} else if (manager.score == 1) {
+			g.drawString("You got 1 point.", 300, 270);
+		} else {
+			g.drawString("You got " + manager.score + " points.", 300 - (scoreDigit() * 5), 270);
+		}
 	}
-	
+
 	void drawICState(Graphics g) {
 		g.setColor(new Color(0, 33, 0));
 		g.fillRect(0, 0, FourPong.length, FourPong.length);
+
+		g.setColor(Color.white);
+		g.setFont(titleFont);
+		g.drawString("Keys", 40, 74);
+		g.drawString("Credits", 440, 74);
+
+		g.setFont(textFont);
+		g.drawString("SPACE: Start and Pause game", 40, 128);
+		g.drawString("V: End game early", 40, 162);
+		g.drawString("B: Instructions & Credits", 40, 196);
+		g.drawString("A & D: Top paddle", 40, 230);
+		g.drawString("W & S: Left paddle", 40, 264);
+		g.drawString("Left & Right: Bottom paddle", 40, 298);
+		g.drawString("Up & Down: Right paddle", 40, 332);
+
+		g.drawString("Me.", 440, 128);
+
+		g.drawLine(400, 0, 400, 800);
+
 	}
 
 	@Override
-	public void paintComponent(Graphics g){
-		if(currentState == titleState) {
+	public void paintComponent(Graphics g) {
+		if (currentState == titleState) {
 			drawTitleState(g);
-		}
-		else if(currentState == gameState) {
+		} else if (currentState == gameState) {
 			drawGameState(g);
-		}
-		else if(currentState == endState) {
+		} else if (currentState == endState) {
 			drawEndState(g);
-		}
-		else if(currentState == icState) {
+		} else if (currentState == icState) {
 			drawICState(g);
 		}
 	}
-	
+
 	void move() {
-		if(isWPressed) {
+		if (isWPressed) {
 			right.y -= right.speed;
 		}
-		if(isSPressed) {
+		if (isSPressed) {
 			right.y += right.speed;
 		}
-		if(isAPressed) {
+		if (isAPressed) {
 			up.x -= up.speed;
 		}
-		if(isDPressed) {
+		if (isDPressed) {
 			up.x += up.speed;
 		}
-		if(isUpPressed) {
+		if (isUpPressed) {
 			left.y -= left.speed;
 		}
-		if(isDownPressed) {
+		if (isDownPressed) {
 			left.y += left.speed;
 		}
-		if(isLeftPressed) {
+		if (isLeftPressed) {
 			down.x -= down.speed;
 		}
-		if(isRightPressed) {
+		if (isRightPressed) {
 			down.x += down.speed;
 		}
 	}
-	
+
 	void ball() {
 		ball.x += ball.xspeed;
 		ball.y += ball.yspeed;
 	}
-	
+
 	void reset() {
 		ball.x = 388;
 		ball.y = 388;
@@ -142,64 +180,66 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
 		down.x = 350;
 		ball.xspeed = 3;
 		ball.yspeed = 1.5;
+		manager.score = 0;
 	}
-	
-	
+
+	int scoreDigit() {
+		String scoreString = manager.score + "";
+		return scoreString.length();
+	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		//System.out.println(e.getKeyCode());
-		
+		// System.out.println(e.getKeyCode());
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println(e.getKeyCode());
-		if(e.getKeyCode() == 87) { //
+		if (e.getKeyCode() == 87) { //
 			isWPressed = true;
-		}
-		else if(e.getKeyCode() == 83) { //
+		} else if (e.getKeyCode() == 83) { //
 			isSPressed = true;
 		}
-		if(e.getKeyCode() == 65) { //
+		if (e.getKeyCode() == 65) { //
 			isAPressed = true;
-		}
-		else if(e.getKeyCode() == 68) { //
+		} else if (e.getKeyCode() == 68) { //
 			isDPressed = true;
 		}
-		if(e.getKeyCode() == 38) { //
+		if (e.getKeyCode() == 38) { //
 			isUpPressed = true;
-		}
-		else if(e.getKeyCode() == 40) { //
+		} else if (e.getKeyCode() == 40) { //
 			isDownPressed = true;
 		}
-		if(e.getKeyCode() == 37) { //
+		if (e.getKeyCode() == 37) { //
 			isLeftPressed = true;
-		}
-		else if(e.getKeyCode() == 39) { //
+		} else if (e.getKeyCode() == 39) { //
 			isRightPressed = true;
 		}
 
-		if(e.getKeyCode() == 32) { //Space
+		if (e.getKeyCode() == 32) { // Space (shift: 16)
 			System.out.println("space");
-			if(currentState == titleState || currentState == endState || currentState == icState) {
+			if (currentState == titleState || currentState == endState || currentState == icState) {
 				currentState = gameState;
 				reset();
 				System.out.println(currentState);
-			}
-			else if(currentState == gameState) {
+			} else if (currentState == gameState) {
 				isPaused = !isPaused;
 			}
 		}
-		if(e.getKeyCode() == 86) { //v
-			if(currentState == titleState || currentState == gameState) {
+		if (e.getKeyCode() == 86) { // v
+			if (currentState == titleState || currentState == gameState) {
 				currentState = endState;
 			}
 		}
-		if(e.getKeyCode() == 66) { //b
-			if(currentState == titleState || currentState == gameState && isPaused || currentState == endState) {
+		if (e.getKeyCode() == 66) { // b
+			if (currentState == titleState || currentState == gameState && isPaused || currentState == endState) {
 				currentState = icState;
+			} else if (currentState == icState) {
+				currentState = titleState;
 			}
 		}
 	}
@@ -207,38 +247,38 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getKeyCode() == 87) { //w
+		if (e.getKeyCode() == 87) { // w
 			isWPressed = false;
 		}
-		if(e.getKeyCode() == 83) { //
+		if (e.getKeyCode() == 83) { //
 			isSPressed = false;
 		}
-		if(e.getKeyCode() == 65) { //
+		if (e.getKeyCode() == 65) { //
 			isAPressed = false;
 		}
-		if(e.getKeyCode() == 68) { //
+		if (e.getKeyCode() == 68) { //
 			isDPressed = false;
 		}
-		if(e.getKeyCode() == 38) { //
+		if (e.getKeyCode() == 38) { //
 			isUpPressed = false;
 		}
-		if(e.getKeyCode() == 40) { //
+		if (e.getKeyCode() == 40) { //
 			isDownPressed = false;
 		}
-		if(e.getKeyCode() == 37) { //
+		if (e.getKeyCode() == 37) { //
 			isLeftPressed = false;
 		}
-		if(e.getKeyCode() == 39) { //
+		if (e.getKeyCode() == 39) { //
 			isRightPressed = false;
 		}
-		
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		repaint();
-		if(currentState == gameState && isPaused == false) {
+		if (currentState == gameState && isPaused == false) {
 			updateGameState();
 		}
 	}
--- src/Manager.java
+++ src/Manager.java
@@ -1,7 +1,7 @@
 import java.awt.Graphics;
 
 public class Manager {
-	
+
 	Paddle right;
 	Paddle left;
 	Paddle up;
@@ -9,7 +9,8 @@ public class Manager {
 	Ball ball;
 	int score;
 	GamePanel panel;
-	
+	int counter = 0;
+
 	public Manager(Paddle r, Paddle l, Paddle u, Paddle d, Ball b, GamePanel p) {
 		right = r;
 		left = l;
@@ -18,7 +19,7 @@ public class Manager {
 		ball = b;
 		panel = p;
 	}
-	
+
 	void update() {
 		right.update();
 		left.update();
@@ -26,8 +27,9 @@ public class Manager {
 		down.update();
 		ball.update();
 		checkCollision();
+		counter++;
 	}
-	
+
 	void draw(Graphics g) {
 		right.draw(g);
 		left.draw(g);
@@ -35,69 +37,85 @@ public class Manager {
 		down.draw(g);
 		ball.draw(g);
 	}
-	
+
 	void checkCollision() {
-		if(right.collisionBox.intersects(ball.collisionBox)) {
+		if (right.collisionBox.intersects(ball.collisionBox)) {
 			ball.xspeed = Math.abs(ball.xspeed);
-			score ++;
-			if(panel.isWPressed) {
-				System.out.println("extra");
-				ball.yspeed = 3;
-				ball.yspeed -= 1.5;
+			if (counter > 3) {
+				score++;
+				counter = 0;
 			}
-			if(panel.isSPressed) {
+			if (panel.isWPressed) {
 				System.out.println("extra");
 				ball.yspeed = 3;
-				ball.yspeed += 1.5;
+				ball.yspeed = 1.5;
+			}
+			if (panel.isSPressed) {
+				System.out.println("extra");
+				ball.yspeed = 3;
+				ball.yspeed = 4.5;
 			}
 		}
-		if(left.collisionBox.intersects(ball.collisionBox)) {
+		if (left.collisionBox.intersects(ball.collisionBox)) {
 			ball.xspeed = -Math.abs(ball.xspeed);
-			score ++;
-			if(panel.isUpPressed) {
-				System.out.println("extra");
-				ball.yspeed = 3;
-				ball.yspeed -= 1.5;
+			if (counter > 3) {
+				score++;
+				counter = 0;
 			}
-			if(panel.isDownPressed) {
+			if (panel.isUpPressed) {
 				System.out.println("extra");
 				ball.yspeed = 3;
-				ball.yspeed += 1.5;
+				ball.yspeed = 1.5;
+			}
+			if (panel.isDownPressed) {
+				System.out.println("extra");
+				ball.yspeed = 3;
+				ball.yspeed = 4.5;
 			}
 		}
-		if(up.collisionBox.intersects(ball.collisionBox)) {
+		if (up.collisionBox.intersects(ball.collisionBox)) {
 			ball.yspeed = Math.abs(ball.yspeed);
-			score ++;
-			if(panel.isAPressed) {
-				System.out.println("extra");
-				ball.xspeed = 3;
-				ball.xspeed -= 1.5;
+			if (counter > 3) {
+				score++;
+				counter = 0;
 			}
-			if(panel.isDPressed) {
+			if (panel.isAPressed) {
 				System.out.println("extra");
 				ball.xspeed = 3;
-				ball.xspeed += 1.5;
+				ball.xspeed = 1.5;
+			}
+			if (panel.isDPressed) {
+				System.out.println("extra");
+				ball.xspeed = 3;
+				ball.xspeed = 4.5;
 			}
 		}
-		if(down.collisionBox.intersects(ball.collisionBox)) {
+		if (down.collisionBox.intersects(ball.collisionBox)) {
 			ball.yspeed = -Math.abs(ball.yspeed);
-			score ++;
-			if(panel.isLeftPressed) {
-				System.out.println("extra");
-				ball.xspeed = 3;
-				ball.xspeed -= 1.5;
+			if (counter > 3) {
+				score++;
+				counter = 0;
 			}
-			if(panel.isRightPressed) {
+			if (panel.isLeftPressed) {
 				System.out.println("extra");
 				ball.xspeed = 3;
-				ball.xspeed += 1.5;
+				ball.xspeed = 1.5;
+			}
+			if (panel.isRightPressed) {
+				System.out.println("extra");
+				ball.xspeed = 3;
+				ball.xspeed = 4.5;
 			}
 		}
-		//if(ball.x ==)
+		if (ball.x <= -20 || ball.x >= 820) {
+			panel.currentState = panel.endState;
+		} else if (ball.y <= -20 || ball.y >= 820) {
+			panel.currentState = panel.endState;
+		}
 	}
-	
+
 	int getScore() {
 		return score;
 	}
-	
+
 }
